b72022ed600da299aea57cdd035002c017ef3123
egeominotti <egeominotti@gmail.com>
2023-03-02 14:21:27 +0100

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL_ARBITRUM, WALLET_PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "arbitrum",
+  networks: {
+    hardhat: {},
+    arbitrum: {
+      url: API_URL_ARBITRUM,
+      accounts: [`0x${WALLET_PRIVATE_KEY}`],
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.17",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
